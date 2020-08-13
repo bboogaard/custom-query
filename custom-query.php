8f2b3a19d91cfaaf33446d3d19ebf3636349c286
bboogaard <padawan@hetnet.nl>
2020-08-13 17:02:50 +0200

    Update release.txt/Bump version

--- custom-query.php
+++ custom-query.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.wp-wikkel.nl/
  * Text Domain:     custom-query
  * Domain Path:     /languages
- * Version:         1.1.0
+ * Version:         1.1.1
  *
  * @package         Custom Query
  */
